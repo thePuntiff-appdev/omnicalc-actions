e63793f2fa27c5012a3feb39c846eb939305ab23
Zak Randall <zrandall@chicagobooth.edu>
2018-10-11 00:28:52 +0000

    Phase 2
    Phase 2 complete w/ all autotests passing

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -10,16 +10,26 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    
+    url = "https://api.darksky.net/forecast/dcdc1364c62099b31d76c0e99232e7c8/#{@lat},#{@lng}"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
 
-    @current_temperature = "Replace this string with your answer"
+    current_temperature = parsed_data.dig("currently", "temperature")
+    current_summary = parsed_data.dig("currently", "summary")
+    summary_of_next_sixty_minutes = parsed_data.dig("minutely", "summary")
+    summary_of_next_several_hours = parsed_data.dig("hourly", "summary")
+    summary_of_next_several_days = parsed_data.dig("daily", "summary")
 
-    @current_summary = "Replace this string with your answer"
+    @current_temperature = "#{current_temperature}" 
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
+    @current_summary = "#{current_summary}"
 
-    @summary_of_next_several_hours = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = "#{summary_of_next_sixty_minutes}"
 
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_hours = "#{summary_of_next_several_hours}"
+
+    @summary_of_next_several_days = "#{summary_of_next_several_days}"
 
     render("forecast_templates/coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -11,16 +11,32 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    goog_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_street_address + "&key=AIzaSyA5qwIlcKjijP_Ptmv46mk4cCjuWhSzS78"
+    goog_raw_data = open(goog_url).read
+    goog_parsed_data = JSON.parse(goog_raw_data)
+    lat = goog_parsed_data.dig("results", 0, "geometry", "location", "lat")
+    lng = goog_parsed_data.dig("results", 0, "geometry", "location", "lng")
+    
+    darksky_url = "https://api.darksky.net/forecast/dcdc1364c62099b31d76c0e99232e7c8/#{lat},#{lng}"
+    darksky_raw_data = open(darksky_url).read
+    darksky_parsed_data = JSON.parse(darksky_raw_data)
 
-    @current_temperature = "Replace this string with your answer"
+    current_temperature = darksky_parsed_data.dig("currently", "temperature")
+    current_summary = darksky_parsed_data.dig("currently", "summary")
+    summary_of_next_sixty_minutes = darksky_parsed_data.dig("minutely", "summary")
+    summary_of_next_several_hours = darksky_parsed_data.dig("hourly", "summary")
+    summary_of_next_several_days = darksky_parsed_data.dig("daily", "summary")
+    
+    
+    @current_temperature = "#{current_temperature}" 
 
-    @current_summary = "Replace this string with your answer"
+    @current_summary = "#{current_summary}"
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = "#{summary_of_next_sixty_minutes}"
 
-    @summary_of_next_several_hours = "Replace this string with your answer"
+    @summary_of_next_several_hours = "#{summary_of_next_several_hours}"
 
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_days = "#{summary_of_next_several_days}"
 
     render("meteorologist_templates/street_to_weather.html.erb")
   end
